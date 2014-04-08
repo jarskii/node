fbeda022947a586241f422ae92f845ff28001dc3
zharsky <jarskii2009@gmail.com>
2014-04-09 00:43:06 +0400

    Add first page

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require("express"),
+    logfmt = require("logfmt"),
+    fs = require('fs'),
+    app = express();
+
 
 app.use(logfmt.requestLogger());
 
+app.use(express.static(__dirname + '/'));
+
 app.get('/', function(req, res) {
-    res.send('Hello Vasia!');
+    fs.readFile('index.html', 'utf8', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
+
+//app.engine('html', require('ejs').renderFile('index.html'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
